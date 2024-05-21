fb06428e6f5dbd487370e7f372d32c5149b72f44
SpadeVer3 <151363024+SpadeVer3@users.noreply.github.com>
2024-05-21 21:21:23 +1000

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <string>
 #include <algorithm> 
-#include <cctype>
 #include <cassert>
+#include <cmath>
 using namespace std;
 
 int factorial(int num) {
@@ -15,17 +14,22 @@ int factorial(int num) {
     return f;
 }
 
-int main() {
-    // Тест факториала нуля
-    assert(factorial(0) == 1);
+int factSqr(int num) {
+    return pow(factorial(num), 2);
+}
 
-    // Тесты факториала положительных чисел
-    assert(factorial(1) == 1);
-    assert(factorial(2) == 2);
-    assert(factorial(3) == 6);
-    assert(factorial(4) == 24);
-    assert(factorial(5) == 120);
-    assert(factorial(10) == 3628800);
+int main() {
+    // Тест квадрата факториала нуля
+    assert(factSqr(0) == 1);
+
+    // Тесты квадратов факториала положительных чисел
+    assert(factSqr(1) == 1);
+    assert(factSqr(2) == 4);
+    assert(factSqr(3) == 36);
+    assert(factSqr(4) == 576);
+    assert(factSqr(5) == 14400);
+
+    cout << "LOXOVKICH" << endl;
 
     return 0;
 }
